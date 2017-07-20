b1d5a46f60effbbf92cbfde2e030cea9a8171076
Juan Sanchez Amador <Juan.Sanchez Amador@chandlersfe.co.uk>
2017-07-20 13:32:37 +0100

    Add new message

--- server/server.js
+++ server/server.js
@@ -14,6 +14,20 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New User connected');
+
+  socket.emit('newMessage', {
+    from: 'Cluj',
+    text: 'Hey what is going on',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createdMessage', message);
+  });
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
+
   socket.on('disconnect', () => {
     console.log('User Disconnected');
   });
