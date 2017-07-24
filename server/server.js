7ca4edf589f0c3965d07aa37adfc23e77986babd
Juan Sanchez Amador <Juan.Sanchez Amador@chandlersfe.co.uk>
2017-07-24 09:48:08 +0000

    Add css

--- server/server.js
+++ server/server.js
@@ -24,13 +24,8 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     console.log('createdMessage', message);
     io.emit('newMessage',generateMessage( message.from, message.text));
-    callback('This is from the server');
+    callback();
 
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //    text: message.text,
-    //    createdAt: new Date().getTime()
-    //  });
   });
 
   socket.on('createLocationMessage', (coords) => {
